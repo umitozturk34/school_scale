f2a5dfbbdf1e818e5ec670eef35cb4fc5581d0cb
Ümit Öztürk <ozturkumit34@gmail.com>
2018-06-25 19:41:10 +0300

    devise fixed.

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,7 @@ class School < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :students
 end
